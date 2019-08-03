e3238644c17fec49337c13c7d041c385865922fc
di_carlo_jonathan <dicarlojonathan@gmal.com>
2019-08-03 16:18:49 -0700

    Added initial page prefabs

---
+++ Assets/Scripts/Pages/Page.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Page : MonoBehaviour
+{
+    public string body = "";
+    Canvas canvas;
+    UiPage page;
+
+
+    private void Start()
+    {
+        canvas = FindObjectOfType<Canvas>();
+        if (canvas == null) {
+            Debug.LogError("Need a page Canvas in scene");
+            return;
+        }
+
+        page = canvas.gameObject.GetComponent<UiPage>();         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (canvas != null)
+        {
+            if (other.gameObject.layer == 8)
+            {
+                page.SetActive(body);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (canvas != null)
+        {
+            if (other.gameObject.layer == 8)
+            {
+                page.SetDeActive();
+            }
+        }
+    }
+}
