727ad296c5b799c31f1aebabc444f0c47ef01b07
Brandon Farrell <Brandon@BPFarrell.com>
2019-08-03 15:51:43 -0700

    Slime deaths!

--- Assets/Scripts/EnemySlime.cs
+++ Assets/Scripts/EnemySlime.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemySlime : MonoBehaviour, IEnemy
 {
     public float moveSpeed;
     public float jumpSpeed = 5;
-
+    Material material;
     public GameObject targat;
     public float scaleFactor = 0.1f;
     float startY;
     float startScale;
+    bool isDying;
+    float timeOfDeath;
     public void Hit()
     {
-        Destroy(gameObject);
+        JuiceManager.Hang(3);
+        isDying = true;
+        timeOfDeath = Time.time;
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material = new Material(mr.material);
+        material = mr.material;
+        Destroy(GetComponent<Collider>());
+        //Destroy(gameObject);
     }
 
     // Start is called before the first frame update
@@ -26,7 +36,12 @@ public class EnemySlime : MonoBehaviour, IEnemy
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isDying)
+        {
+            Dying();
+            return;
+        }
         Vector3 pos = transform.position;
         float sin = Mathf.Clamp01(Mathf.Sin(Time.time * jumpSpeed)) * 0.1f;
         pos.y = startY;
@@ -39,5 +54,17 @@ public class EnemySlime : MonoBehaviour, IEnemy
         Vector3 scale = transform.localScale;
         scale.y = startScale + sin * scaleFactor;
         transform.localScale = scale;
+    }
+    void Dying()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x += Time.deltaTime*1;
+        scale.y += Time.deltaTime*1;
+        scale.z += Time.deltaTime*1;
+        transform.localScale = scale;
+        float t = (Time.time - timeOfDeath)*3;
+        material.SetFloat("_Death", t);
+        if (t > 1)
+            Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/JuiceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuiceManager : MonoBehaviour
+{
+    private static JuiceManager instance;
+    private int framesLeftFrozen = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        if (framesLeftFrozen == 0) return;
+        framesLeftFrozen--;
+
+        if(framesLeftFrozen == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public static void Hang(int frames)
+    {
+        Time.timeScale = 0;
+        instance.framesLeftFrozen = frames + 1;
+    }
+}
