5182eea59c0d9a44989daa27e6b1d4e7d70bc21e
unknown <georgeb@bentimagelab.com>
2019-08-02 20:57:41 -0700

    Add: Room and CardinalRooms classes

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CardinalRooms
+{
+    Room nw = null;
+    Room ne = null;
+    Room se = null;
+    Room sw = null;
+}
+
 public class Room : MonoBehaviour
 {
+
+    CardinalRooms rooms = new CardinalRooms();
+    
     // Start is called before the first frame update
     void Start()
     {
