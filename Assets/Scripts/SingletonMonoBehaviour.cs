ce6161346b2c8bb327f96bb940457c2b2a31f61f
JasonCrossley <rojibora@gmail.com>
2019-08-03 12:00:18 -0700

    SingletonMonoBehobehaviour

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+{
+    private static T _Instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (_Instance != null) return _Instance;
+            T[] runtimes = FindObjectsOfType<T>();
+            if (runtimes.Length > 1) {
+                Debug.LogError("Multiple RuntimeManager found in scene");
+                for (int i = runtimes.Length - 1; i > 0; i--) {
+                    Destroy(runtimes[i].gameObject);
+                }
+            }
+            if (runtimes.Length == 0)
+            {
+                Debug.LogError("No RuntimeManager found in scene");
+                return null;
+            }
+            _Instance = runtimes[0];
+            return _Instance;
+        }
+    }
+}
