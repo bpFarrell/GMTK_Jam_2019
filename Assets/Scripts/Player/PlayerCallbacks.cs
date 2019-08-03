d74278240bb3d7aeb5df9d197af80a7ddc697125
di_carlo_jonathan <dicarlojonathan@gmal.com>
2019-08-02 22:41:16 -0700

    player movement example scene

---
+++ Assets/Scripts/Player/PlayerCallbacks.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCallbacks : MonoBehaviour
+{
+    public static Action<GameObject> PlayerHitByEnemy;
+}
