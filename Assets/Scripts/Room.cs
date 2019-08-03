bcc335539f381e03f81ee6631036308752e49ed1
JasonCrossley <rojibora@gmail.com>
2019-08-02 20:02:18 -0700

    Testing discord git event

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -7,7 +7,7 @@ public class Room : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Is it Gitting now?");
     }
 
     // Update is called once per frame
