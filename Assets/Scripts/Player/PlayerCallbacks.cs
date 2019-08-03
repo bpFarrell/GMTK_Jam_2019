c75afae4b356c85188794ca5c4de2ac2ee902788
di_carlo_jonathan <dicarlojonathan@gmal.com>
2019-08-03 00:27:13 -0700

    Added player flame distance callbacks

--- Assets/Scripts/Player/PlayerCallbacks.cs
+++ Assets/Scripts/Player/PlayerCallbacks.cs
@@ -6,4 +6,9 @@ using UnityEngine;
 public class PlayerCallbacks : MonoBehaviour
 {
     public static Action<GameObject> PlayerHitByEnemy;
+    public static Action PlayeEnteredDarknes;
+    public static Action PlayerEnteredLight;
+    public static Action<float> PlayerStayedInDarkness; 
+    
+
 }
---
+++ Assets/Scripts/Player/PlayerFlameInteractionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlameInteractionManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        PlayerCallbacks.PlayeEnteredDarknes += EnteredDark;
+        PlayerCallbacks.PlayerEnteredLight += EnteredLight;
+        PlayerCallbacks.PlayerStayedInDarkness += StayedInDark;
+
+    }
+
+    private void OnDisable()
+    {
+
+        PlayerCallbacks.PlayeEnteredDarknes -= EnteredDark;
+        PlayerCallbacks.PlayerEnteredLight -= EnteredLight;
+        PlayerCallbacks.PlayerStayedInDarkness -= StayedInDark;
+    }
+
+    private void EnteredDark()
+    {
+        Debug.LogFormat("Player EnteredDark ");
+    }
+    private void StayedInDark(float timeInDark)
+    {
+        Debug.LogFormat("Player stayedInDark for: {0}", timeInDark);
+    }
+    private void EnteredLight()
+    {
+        Debug.LogFormat("Player enteredLight ");
+    }
+}
