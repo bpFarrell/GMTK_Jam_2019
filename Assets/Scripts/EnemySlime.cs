a5af3ed06f5d1b4f623a6567709cbbcf4280b7ec
Brandon Farrell <Brandon@BPFarrell.com>
2019-08-02 23:20:12 -0700

    Slime time!

---
+++ Assets/Scripts/EnemySlime.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySlime : MonoBehaviour, IEnemy
+{
+    public float moveSpeed;
+    public float jumpSpeed = 5;
+
+    public GameObject targat;
+    public float scaleFactor = 0.1f;
+    float startY;
+    float startScale;
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+        startScale = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float sin = Mathf.Clamp01(Mathf.Sin(Time.time * jumpSpeed)) * 0.1f;
+        pos.y = startY;
+        if (sin > 0)
+        {
+            pos = Vector3.MoveTowards(pos, targat.transform.position, moveSpeed * Time.deltaTime);
+        }
+        pos.y += sin;
+        transform.position = pos;
+        Vector3 scale = transform.localScale;
+        scale.y = startScale + sin * scaleFactor;
+        transform.localScale = scale;
+    }
+}
