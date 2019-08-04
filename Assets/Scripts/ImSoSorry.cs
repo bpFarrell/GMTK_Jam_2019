729aaa7cc65d76beee7739621d1bc556c179a511
JasonCrossley <rojibora@gmail.com>
2019-08-04 08:19:45 -0700

    Room rc1

---
+++ Assets/Scripts/ImSoSorry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ImSoSorry : MonoBehaviour
+{
+    public List<string> appologyList = new List<string>();
+    private Queue<string> appologyQueue = new Queue<string>();
+
+    public float delay = 4f;
+    private float count = 0f;
+
+    private void OnEnable()
+    {
+        appologyQueue.Clear();
+        foreach (string s in appologyList) {
+            appologyQueue.Enqueue(s);
+        }
+    }
+
+    private void Update()
+    {
+        if (appologyList.Count == 0) return;
+        
+        count += Time.deltaTime;
+        if (count <= delay) return;
+        
+        count -= delay;
+        VSFXLogic vsfx = Instantiate(Resources.Load<VSFXLogic>("VSFXSorry"), transform);
+        vsfx.sound = appologyQueue.Dequeue();
+    }
+}
